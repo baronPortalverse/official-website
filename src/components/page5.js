e02073262cf174729a86aba8a880e7b6bf18696e
Baron <96877531+baronPortalverse@users.noreply.github.com>
2022-07-06 17:25:56 +0800

    Update page5.js

--- src/components/page5.js
+++ src/components/page5.js
@@ -4,6 +4,7 @@ import p2 from '../assets/imgs/Fusotau.png'
 import p3 from '../assets/imgs/Vector.png'
 import p4 from '../assets/imgs/big-brain-investments 1.png'
 import p5 from '../assets/imgs/Cogitent-Ventures-Horizontal-Light-Transparent 1.png'
+import p6 from '../assets/imgs/tomooncapital.png'
 import { lang } from '../lang'
 
 /**
@@ -27,7 +28,7 @@ function page5(root) {
 	const boxContainer = document.createElement('div')
 	boxContainer.className = style.boxContainer
 	container.appendChild(boxContainer)
-	;[p1, p2, p3, p4, p5].forEach((p) => {
+	;[p1, p2, p3, p4, p5, p6].forEach((p) => {
 		const pic = document.createElement('img')
 		pic.src = p
 		boxContainer.appendChild(pic)
