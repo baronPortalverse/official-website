df47abd9d04473964ea59e3a66692f7db8fd26d7
harrisonworks <harrisonadmcewen@gmail.com>
2022-07-14 19:28:09 +0100

    added navigation

---
+++ components/NavBar.vue
@@ -0,0 +1,86 @@
+<template>
+	<nav>
+		<div class="container p-3">
+			<div class="d-flex align-items-center">
+				<div class="logo">
+					<img src="~/assets/images/logo-tagline.png" alt="" />
+				</div>
+
+				<div id="links" class="ms-auto">
+					<a
+						class="px-2"
+						href="https://google.com"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img src="~/assets/images/icons/medium.svg" alt="Medium" />
+					</a>
+					<a
+						class="px-2"
+						href="https://google.com"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img src="~/assets/images/icons/discord.svg" alt="Discord" />
+					</a>
+					<a
+						class="px-2"
+						href="https://google.com"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img src="~/assets/images/icons/twitter.svg" alt="Twitter" />
+					</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+nav {
+	position: fixed;
+	top: 0px;
+	z-index: 10;
+	width: 100vw;
+
+	background: linear-gradient(
+		180deg,
+		#1717177a 22.92%,
+		rgba(23, 23, 23, 0) 100%
+	);
+	mix-blend-mode: normal;
+	backdrop-filter: blur(20px);
+}
+ul {
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	background-color: #333;
+}
+
+li {
+	float: left;
+}
+
+li a {
+	display: block;
+	color: white;
+	text-align: center;
+	padding: 14px 16px;
+	text-decoration: none;
+}
+
+li a:hover:not(.active) {
+	background-color: #111;
+}
+
+.active {
+	background-color: #04aa6d;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<nav-bar />
+		<nuxt />
+	</div>
+</template>
+
+
+<script>
+export default {
+	name: 'DefaultLayout',
+}
+</script>
+
+<style>
+</style>
